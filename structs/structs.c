ea7d91dafa321f78c39bf584b581217aecbca599
Paul Haddad <paulh16@gmail.com>
2021-01-15 07:22:47 -0500

    Structs

--- structs/structs.c
+++ structs/structs.c
@@ -8,7 +8,10 @@
     Person should have the fields `name`, `age`, `height`, and `weight`.
 */
 typedef struct Person {
-
+  char *name;
+  int age;
+  int height;
+  int weight;
 } Person;
 
 /*
@@ -19,18 +22,25 @@ typedef struct Person {
     When setting the `name` field, use your previously-built string_dup
     function, which handles the allocation of memory for the new string.
 */
-Person *createPerson(char *name, int age, int height, int weight)
-{
+Person *createPerson(char *name, int age, int height, int weight) {
+  Person *newPerson = malloc(sizeof(Person));
+  newPerson->name = malloc(strlen(name));
+  strcpy(newPerson->name, name);
 
+  newPerson->age = age;
+  newPerson->height = height;
+  newPerson->weight = weight;
+
+  return newPerson;
 }
 
 /*
     Given a pointer to a Person struct, frees up the memory that holds the
     Person's name as well as the memory that holds the Person instance.
 */
-void destroyPerson(Person *who)
-{
-
+void destroyPerson(Person *who) {
+  free(who->name);
+  free(who);
 }
 
 #ifndef TESTING
